90322f29ead2bd61ee7cb33648f73a91f843dd57
Joule-Flow <github.julian@flores-it.de>
2019-10-27 08:04:35 +0100

    Moved Navigation into defined controller space.

--- src/Controller/Planning/ScheduleControllerInterface.php
+++ src/Controller/Planning/ScheduleControllerInterface.php
@@ -4,7 +4,7 @@ namespace App\Controller\Planning;
 
 use App\Services\requestingPassengerDTOInterface;
 
-interface ScheduleInterface
+interface ScheduleControllerInterface
 {
     public function index(requestingPassengerDTOInterface $dto);
 }
